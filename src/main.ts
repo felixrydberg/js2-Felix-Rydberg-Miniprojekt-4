0483fd4c37639bf85dd85b4c82dc8d3536b6ed65
felixrydberg <felixryd@gmail.com>
2022-03-09 13:21:47 +0100

    [Add] Planing in README

---
+++ src/main.ts
@@ -0,0 +1,45 @@
+class Tamagochi {
+    private readonly name:string;
+    private readonly type:string;
+    private happiens:number;
+    private hungry:number;
+    private poopcounter;
+
+    constructor() {
+        let names:string[] = ["Felix", "Tuva", "Ann", "Fredrik", "Maria", "David"];
+        let types:string[] = ["Hund", "Kanin", "Katt", "Hamster", "Fladdermus", "Råtta"];
+
+        this.name = names[Math.floor(Math.random() * names.length)]
+        this.type = types[Math.floor(Math.random() * types.length)]
+        this.happiens = 5;
+        this.hungry = 5;
+
+    
+    
+    }
+    feed():void {
+        if(this.hungry === 0) {
+            // - happines
+        }
+        else {
+            this.hungry--
+        }
+    }
+    play():void {
+        if(this.happiens === 10) {
+            // + hungry
+        }
+        else {
+            this.happiens++
+        }
+    }
+    minusValue(target: 'happiens'| 'hungry', action: '+'|'-'):void {
+
+    }
+    plusValue(target: 'happiens'| 'hungry', action: '+'|'-'):void {
+
+    }
+
+
+
+}
